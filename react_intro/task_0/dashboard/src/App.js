81a60f0b6aade2d166c0ff36634031b63e96f672
Georgia2292 <georgelina2292@gmail.com>
2024-06-17 02:07:10 +0000

    Basic application

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import logo from './logo.jpg'
+import logo from './holberton-logo.jpg';
+import './App.css';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div className="App">
-	  			<header className="App-header">
-		  			<img src={logo} className="App-logo" alt="logo" />
-		  			<h1>School dashboard</h1>
-	  			</header>
-	 			<body className="App-body">
-		 		 	<p>Login to access the full dashboard</p>
-	  			</body>
-				<footer className="App-footer">
-					<p>Copyright 2020 - holberton School</p>
-				</footer>
-			</div>
-		)
-	}
+function App() {
+  return (
+    <>
+    <div className="App-header">
+      <img src={logo} alt="Holberton Logo: Red Seahorse"/>
+      <h1>School dashboard</h1>
+    </div>
+      <div className="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+      <div className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+    </>
+  );
 }
+
+export default App;
