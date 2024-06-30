573441fb563ffa6c74ba827d21c72bc766a2d07a
Georgia2292 <georgelina2292@gmail.com>
2024-06-30 21:35:42 +0000

    Basic components

--- react_props/task_0/dashboard/src/Header/Header.js
+++ react_props/task_0/dashboard/src/Header/Header.js
@@ -1,14 +1,12 @@
-import logo from '../assets/holberton-logo.jpg';
-import './Header.css';
-import React from 'react';
+import React, { Component } from 'react'
+import logo from '../assets/logo.jpg'
+import './Header.css'
 
-function Header() {
-    return (
-        <div className="App-header">
-            <img src={ logo } alt="Holberton Logo: Red Seahorse" />
-            <h1>School dashboard</h1>
-        </div>
-    )
+export default function Header(props) {
+	return (
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h1>School dashboard</h1>
+		</header>
+	)
 }
-
-export default Header;
--- react_props/task_0/dashboard/src/Login/Login.js
+++ react_props/task_0/dashboard/src/Login/Login.js
@@ -1,25 +1,27 @@
-import './Login.css';
-import React from 'react';
+import React, { Component } from 'react'
+import './Login.css'
 
-function Login() {
-    return (
-        <div className="App-body">
-            <p>Login to access the full dashboard</p>
-            <div className="form">
-                <label htmlFor="email">
-                    <span>Email:</span>
-                    <input type="email" name="email" id="email" />
-                </label>
-
-                <label htmlFor="password">
-                    <span>Password:</span>
-                    <input type="password" name="password" id="pwd" />
-                </label>
-
-                <button onClick={ () => { } }>OK</button>
-            </div>
-        </div>
-    );
+export default class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="App">
+					<body className="App-body">
+						<p>Login to access the full dashboard</p>
+						<label htmlFor="email" onClick={() => {
+							// select corresponding input
+							document.getElementById('password').focus();
+						}}>Email</label>
+						<input type="email" id="email" />
+						<label htmlFor="password" onClick={() => {
+							// select corresponding input
+							document.getElementById('password').focus();
+						}}>Password</label>
+						<input type="password" id="password" />
+						<button>OK</button>
+					</body>
+				</div>
+			</React.Fragment>
+		)
+	}
 }
-
-export default Login;
